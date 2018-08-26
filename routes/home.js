f068f58b561d673ef6a442cfb5e85283db66e316
vughelli <vduk225@gmail.com>
2018-08-26 12:09:39 -0400

    Update Home route.
    Signed-off-by: vughelli <vduk225@gmail.com>

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,5 @@
 const express = require('express');
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.send({
-    message: 'Please go to: /contents',
-  });
-});
+router.get('/', (req, res) => res.type('text/html').send('<h1>303 Software NodeJS Test</h1><p>Please go to /contents</p>'));
 module.exports = router;
